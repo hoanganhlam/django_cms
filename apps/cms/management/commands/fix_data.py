0e245ba98f61f7ad41d122cc954b9d37df0abc03
lamha <lam@trip.vn>
2021-01-23 17:12:34 +0700

    Update Requirements.txt

--- apps/cms/management/commands/fix_data.py
+++ apps/cms/management/commands/fix_data.py
@@ -5,12 +5,26 @@ import requests
 from django.contrib.auth.models import User
 from utils.instagram import get_comment, fetch_avatar
 from apps.media.models import Media
+from django.db.models import Count
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        tax_terms = PublicationTerm.objects.filter(publication__id=7, taxonomy="species", term__title__startswith="/java/")
-        print(tax_terms.count())
+        tax_terms = PublicationTerm.objects.annotate(count_related=Count('related')).filter(
+            publication__id=7,
+            count_related=1,
+            taxonomy="species",
+            related__taxonomy="genus"
+        )
         for term in tax_terms:
-            print(term.id)
-            term.delete()
+            for related in term.related.all():
+                all_genus_related = related.related.all()
+                for g_related in all_genus_related:
+                    if g_related not in term.related.all():
+                        term.related.add(g_related)
+            if term.options is None:
+                term.options = {}
+            term.options["is_primary"] = True
+            term.save()
+            print(term.term.title)
+
