7b4564c7d5767f09ffe44d4f68bc5feca68eb036
lamha <lam@trip.vn>
2021-01-23 23:39:48 +0700

    Update Requirements.txt

--- apps/cms/management/commands/fix_data.py
+++ apps/cms/management/commands/fix_data.py
@@ -13,28 +13,29 @@ class Command(BaseCommand):
         q = Q(
             publication__id=7,
             taxonomy="species",
-        ) & Q(related__taxonomy="genus") & ~Q(related__taxonomy="phylum")
+        ) & ~Q(related__taxonomy="phylum") & ~Q(related__taxonomy="genus") & ~Q(related__taxonomy="family")
         tax_terms = PublicationTerm.objects.annotate(count_related=Count("related")).filter(q)
         print(tax_terms.count())
+
         for term in tax_terms:
-            for related in term.related.filter(taxonomy="genus"):
-                all_genus_related = related.related.all()
-                for g_related in all_genus_related:
-                    if g_related not in term.related.all():
-                        term.related.add(g_related)
-            if term.options is None:
-                term.options = {}
-            term.options["is_primary"] = False
-            term.save()
+            # for related in term.related.filter(taxonomy="genus"):
+            #     all_genus_related = related.related.all()
+            #     for g_related in all_genus_related:
+            #         if g_related not in term.related.all():
+            #             term.related.add(g_related)
+            # if term.options is None:
+            #     term.options = {}
+            # term.options["is_primary"] = False
+            # term.save()
 
             # for x in term.parent.related.all():
             #     if x not in term.related.all():
             #         term.related.add(x)
 
-            # if term.options is None:
-            #     term.options = {}
-            # term.options["is_subversion"] = True
-            # term.save()
+            if term.options is None:
+                term.options = {}
+            term.options["is_subversion"] = True
+            term.save()
 
             # for related in term.related.filter(taxonomy="class"):
             #     term.parent = related
