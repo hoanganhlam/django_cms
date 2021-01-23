e69bdb39c9712009e2d552f3f880e9cf82f935d7
lamha <lam@trip.vn>
2021-01-23 21:37:47 +0700

    Update Requirements.txt

--- apps/cms/management/commands/fix_data.py
+++ apps/cms/management/commands/fix_data.py
@@ -10,14 +10,19 @@ from django.db.models import Count, Q
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
+        for x in PublicationTerm.objects.filter(publication__id=7, term__title__startswith="/java/"):
+            print(x.term.title)
+            x.delete()
+        return
         q = Q(
             publication__id=7,
-            taxonomy="family",
-            related__taxonomy="family"
-        ) & Q(count_related=1)
+            taxonomy="tribe",
+            # related__taxonomy="tribe"
+            parent__isnull=False
+        ) & ~Q(count_related=1) & ~Q(related__taxonomy="phylum")
         tax_terms = PublicationTerm.objects.annotate(count_related=Count("related")).filter(q)
         for term in tax_terms:
-            # for related in term.related.all():
+            # for related in term.related.filter(taxonomy="family"):
             #     all_genus_related = related.related.all()
             #     for g_related in all_genus_related:
             #         if g_related not in term.related.all():
@@ -27,10 +32,14 @@ class Command(BaseCommand):
             # term.options["is_primary"] = True
             # term.save()
 
-            if term.options is None:
-                term.options = {}
-            term.options["is_subversion"] = True
-            term.save()
+            for x in term.parent.related.all():
+                if x not in term.related.all():
+                    term.related.add(x)
+
+            # if term.options is None:
+            #     term.options = {}
+            # term.options["is_subversion"] = True
+            # term.save()
 
             # for related in term.related.filter(taxonomy="class"):
             #     term.parent = related
