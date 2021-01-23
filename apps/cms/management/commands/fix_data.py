4b8b66cf214fcf0eb31b737a42054cc6c7007e76
lamha <lam@trip.vn>
2021-01-23 16:00:38 +0700

    Update Requirements.txt

--- apps/cms/management/commands/fix_data.py
+++ apps/cms/management/commands/fix_data.py
@@ -9,9 +9,8 @@ from apps.media.models import Media
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        tax_terms = PublicationTerm.objects.filter(publication__id=7, taxonomy="clade", term__slug="magnoliopsida")
-        print(tax_terms)
+        tax_terms = PublicationTerm.objects.filter(publication__id=7, taxonomy="species", term__title__startswith="/java/")
+        print(tax_terms.count())
         for term in tax_terms:
-            term.taxonomy = "class"
-            term.save()
-            print(term)
+            print(term.id)
+            term.delete()
