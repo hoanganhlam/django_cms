1a48068c4fb233238b6dde8cb2ea6cc9616c61ea
lamha <lam@trip.vn>
2021-01-26 18:20:01 +0700

    Update Requirements.txt

--- utils/web_checker.py
+++ utils/web_checker.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import re
 
 
 def get_description(lang, search, limit):
@@ -72,3 +73,51 @@ def get_keyword_suggestion(keyword):
         for x in soup.find_all('suggestion'):
             out.append(x.get("data"))
     return out
+
+
+def crawl_retriever_link():
+    r = requests.get("https://www.factretriever.com/hashtags/kidfriendly")
+    soup = BeautifulSoup(r.content, features="html.parser")
+    elms = soup.select(".tagArticle a")
+    return list(map(lambda a: "https://www.factretriever.com" + a.get("href"), elms))
+
+
+def crawl_retriever(url):
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, features="html.parser")
+    preferences = {}
+    tags = []
+    output = []
+    # get questions
+    # for elm in soup.select("#ddivFrequentlyAskedQuestion .divQuestionsAnswer"):
+    #     print(elm)
+
+    # Instance name
+    elms = soup.select(".customBreadcrumb li")
+    title = elms[len(elms) - 1].find_all(text=True)[0].replace(" Facts", "")
+    if "Top 10" in title:
+        return []
+    tags.append(title)
+    # get tags
+    for elm in soup.select("#ContentPlaceHolder1_divInnerHashTag a"):
+        tags.append(re.sub(r"(\w)([A-Z])", r"\1 \2", elm.text.replace("#", "")))
+    # Get references
+    for elm in soup.select(".divReferences .pReference"):
+        sup = str(elm.contents[0].find_all(text=True))
+        elm.contents[0].decompose()
+        preferences[re.search(r"\[([A-Za-z0-9_]+)\]", sup.replace("'", "")).group(1)] = str(elm.decode_contents())
+    # Get items
+    for elm in soup.select(".factsList li"):
+        keys = []
+        for sup in elm.select("sub"):
+            keys = keys + re.findall(r"\[([A-Za-z0-9_]+)\]", str(sup).replace("'", ""))
+        for x in elm.find_all('sub'):
+            x.decompose()
+        output.append({
+            "id": elm.get("id"),
+            "title": title,
+            "tags": tags,
+            "content": "".join(elm.find_all(text=True)),
+            "refers": list(map(lambda a: preferences.get(a), keys))
+        })
+    return output
