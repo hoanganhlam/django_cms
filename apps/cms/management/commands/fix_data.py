880fed4dba382620260ff0576d7f56e2cfc2ddab
Son Tung <tungds.upwork@gmail.com>
2021-03-25 00:30:54 +0700

    Update Requirements.txt

--- apps/cms/management/commands/fix_data.py
+++ apps/cms/management/commands/fix_data.py
@@ -1,50 +1,12 @@
 from django.core.management.base import BaseCommand
-from apps.cms.models import Post, Term, SearchKeyword, Publication, PublicationTerm
-from utils.web_checker import get_keyword_suggestion
-import requests
-from django.contrib.auth.models import User
-from utils.instagram import get_comment, fetch_avatar
-from apps.media.models import Media
-from django.db.models import Count, Q
+from apps.cms.models import PublicationTerm
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        q = Q(
-            publication__id=7,
-            taxonomy="species",
-        ) & Q(count_related=0)
-        tax_terms = PublicationTerm.objects.annotate(count_related=Count("related")).filter(q)
-
-        spathiphyllum = PublicationTerm.objects.get(taxonomy="genus", term__slug="spathiphyllum")
-        all_genus_related = list(spathiphyllum.related.all())
-        for term in tax_terms:
-            for g_related in (all_genus_related + [spathiphyllum]):
-                if g_related not in term.related.all():
-                    term.related.add(g_related)
-
-            # for related in term.related.filter(taxonomy="genus"):
-            #     all_genus_related = related.related.all()
-            #     for g_related in all_genus_related:
-            #         if g_related not in term.related.all():
-            #             term.related.add(g_related)
-            if term.options is None:
-                term.options = {}
-            term.options["is_primary"] = False
-            term.save()
-
-            # for x in term.parent.related.all():
-            #     if x not in term.related.all():
-            #         term.related.add(x)
-
-            # if term.options is None:
-            #     term.options = {}
-            # term.options["is_subversion"] = True
-            # term.save()
-
-            # for related in term.related.filter(taxonomy="class"):
-            #     term.parent = related
-            #     term.related.remove(related)
-            #     term.save()
-
-            print(term.term.title)
+        tillandsia = PublicationTerm.objects.get(taxonomy="genus", term__slug="tillandsia")
+        plants = tillandsia.posts.filter(post_type="plant")
+        for plant in plants:
+            plant.meta["soil"] = None
+            plant.save()
+            print(plant)
