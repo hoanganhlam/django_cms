a20115015d37e9b1623e6821ed25b6f526259f69
Son Tung <tungds.upwork@gmail.com>
2021-07-07 22:49:34 +0700

    Update Requirements.txt

--- apps/cms/management/commands/make_cache.py
+++ apps/cms/management/commands/make_cache.py
@@ -9,12 +9,13 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         pub = Publication.objects.get(pk=kwargs["pub"])
-        posts = pub.posts.order_by("-id").filter(show_cms=True)
-        terms = pub.terms.order_by("-id").filter(show_cms=True)
+        posts = pub.pp_posts.order_by("-id").filter(show_cms=True)
+        terms = pub.pub_terms.order_by("-id").filter(show_cms=True)
         for item in posts:
             caching_v2.make_post(pub.host, {
                 "instance": item.id
             }, True)
+            print(item.title)
         for item in terms:
             caching_v2.make_term(pub.host, {
                 "instance": item.id
