97b912c1b44efde3df8230ccea7cacc57b88dfe1
Son Tung <tungds.upwork@gmail.com>
2021-07-24 08:29:16 +0700

    Update Requirements.txt

--- apps/cms/management/commands/import_instagram.py
+++ apps/cms/management/commands/import_instagram.py
@@ -37,54 +37,55 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         pub = Publication.objects.get(pk=31)
         admin = User.objects.get(pk=1)
-        results = cur.execute("SELECT * FROM ig_posts WHERE tags LIKE '%monsteradeliciosavariegata%'").fetchall()
-        for r in results:
-            pk = r[0]
-            item = json.loads(r[1])
-            instance = Post.objects.filter(meta__ig_id=pk).first()
-            if instance is None and len(item.get("images", [])) > 0:
-                user = sync_ig_user(item.get("user"))
-                medias = []
-                for img in item.get("images", []):
-                    media = Media.objects.save_url(img)
-                    medias.append(media.id)
-                meta = {
-                    "ig_id": pk,
-                    "credit": item.get("user").get("username"),
-                    "medias": medias
-                }
-                new_post = Post.objects.create(
-                    title="Post by " + item.get("user").get("full_name") if item.get("user").get(
-                        "full_name") else item.get("user").get("username"),
-                    description=item.get("caption")[:300] if item.get("caption") else None,
-                    meta=meta,
-                    primary_publication=pub,
-                    user=user if user is not None else admin,
-                    post_type="post",
-                    show_cms=True,
-                    status="POSTED"
-                )
-                for tag in item.get("tags", []):
-                    if len(tag) < 100:
-                        try:
-                            term, is_created = Term.objects.get_or_create(slug=tag, defaults={
-                                "title": tag
-                            })
-                            pub_term, is_created = PublicationTerm.objects.get_or_create(
-                                publication=pub, term=term,
-                                defaults={
-                                    "taxonomy": "tag"
+        for t in ["ficusaltissimavariegata", "ficuspumilavariegata", "ficuslyratavarirgata", "ficustriangularisvariegata"]:
+            results = cur.execute("SELECT * FROM ig_posts WHERE tags LIKE '%{}%'".format(t)).fetchall()
+            for r in results:
+                pk = r[0]
+                item = json.loads(r[1])
+                instance = Post.objects.filter(meta__ig_id=pk).first()
+                if instance is None and len(item.get("images", [])) > 0:
+                    user = sync_ig_user(item.get("user"))
+                    medias = []
+                    for img in item.get("images", []):
+                        media = Media.objects.save_url(img)
+                        medias.append(media.id)
+                    meta = {
+                        "ig_id": pk,
+                        "credit": item.get("user").get("username"),
+                        "medias": medias
+                    }
+                    new_post = Post.objects.create(
+                        title="Post by " + item.get("user").get("full_name") if item.get("user").get(
+                            "full_name") else item.get("user").get("username"),
+                        description=item.get("caption")[:300] if item.get("caption") else None,
+                        meta=meta,
+                        primary_publication=pub,
+                        user=user if user is not None else admin,
+                        post_type="post",
+                        show_cms=True,
+                        status="POSTED"
+                    )
+                    for tag in item.get("tags", []):
+                        if len(tag) < 100:
+                            try:
+                                term, is_created = Term.objects.get_or_create(slug=tag, defaults={
+                                    "title": tag
                                 })
-                            new_post.terms.add(pub_term)
-                            plants = Post.objects.filter(
-                                terms__taxonomy="tag",
-                                terms__term__slug=tag,
-                                post_type="plant",
-                                primary_publication=pub
-                            )
-                            for plant in plants:
-                                new_post.post_related.add(plant)
-                        except Exception as e:
-                            print(e)
-                            pass
-                print(new_post.id)
+                                pub_term, is_created = PublicationTerm.objects.get_or_create(
+                                    publication=pub, term=term,
+                                    defaults={
+                                        "taxonomy": "tag"
+                                    })
+                                new_post.terms.add(pub_term)
+                                plants = Post.objects.filter(
+                                    terms__taxonomy="tag",
+                                    terms__term__slug=tag,
+                                    post_type="plant",
+                                    primary_publication=pub
+                                )
+                                for plant in plants:
+                                    new_post.post_related.add(plant)
+                            except Exception as e:
+                                print(e)
+                                pass
+                    print(new_post.id)
